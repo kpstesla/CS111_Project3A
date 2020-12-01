07351771f9d81f375846f33c069fd9fdb0f7454e
kpstesla <kswhitecross@gmail.com>
2020-11-30 21:17:24 -0500

    create driver code

--- lab3a.c
+++ lab3a.c
@@ -3,13 +3,41 @@
 // ID: 905381434, 
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
 
 #include "ext2_fs.h"
 #include "lab3a.h"
 
-int main(int argc, const char * argv[])
-{
+// relevant globals
+const char* filepath;
+int fs_image;
+
+// helper functions
+void usage() {
+    fprintf(stderr, "usage: lab3a ext2_img\n");
+}
+
+
+int main(int argc, const char * argv[]) {
     // parse arguments
+    if (argc != 2) {
+        fprintf(stderr, "illegal number of arguments\n");
+        usage();
+        exit(1);
+    }
+    
+    filepath = argv[1];
+    fs_image = open(filepath, O_RDONLY);
+    if (fs_image < 0) {
+        fprintf(stderr, "can't open file %s: %s\n", filepath, strerror(errno));
+        usage();
+        exit(1);
+    }
+    
+    
     // initialize variables
     // parse superblock
     // parse group
