12cc299930d998b7fa248e15f93d73d081923747
kpstesla <kswhitecross@gmail.com>
2020-12-01 09:45:15 -0500

    finish parse_groups

--- kyle.c
+++ kyle.c
@@ -3,8 +3,14 @@
 // ID: 905381434, 
 
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
 #include "ext2_fs.h"
 #include "lab3a.h"
+
+extern int fs_image;
+extern int block_size;
 
 void parse_super(struct ext2_super_block *super, struct ext2_group_desc *group) {
     //title
@@ -15,7 +21,6 @@ void parse_super(struct ext2_super_block *super, struct ext2_group_desc *group) 
     printf("%d,", super->s_inodes_count);
     
     //block and inode size
-    int block_size = EXT2_MIN_BLOCK_SIZE << super->s_log_block_size;
     printf("%d,", block_size);
     printf("%d,", super->s_inode_size);
     
@@ -29,11 +34,34 @@ void parse_super(struct ext2_super_block *super, struct ext2_group_desc *group) 
 }
 
 void parse_groups(struct ext2_super_block *super, struct ext2_group_desc *group) {
+    //title
+    printf("GROUP,");
+    printf("%d,", 0);
+    
+    //total number of blocks and inodes in this group
+    int group_blocks = super->s_blocks_count;
+    int group_inodes = super->s_inodes_count;
+    printf("%d,", group_blocks);
+    printf("%d,", group_inodes);
+    
+    //free blocks and inodes
+    printf("%d,", group->bg_free_blocks_count);
+    printf("%d,", group->bg_free_inodes_count);
+    
+    //block ids for block and inode bitmaps
+    printf("%d,", group->bg_block_bitmap);
+    printf("%d,", group->bg_inode_bitmap);
+    
+    //block id for inode table
+    printf("%d\n", group->bg_inode_table);
     return;
 }
 
 void parse_inodes_free(struct ext2_super_block *super, struct ext2_group_desc *group) {
-    return;
+    //get the bitmap
+    int bitmap_id = group->bg_inode_bitmap;
+    unsigned long* i_bitmap = (unsigned long*) malloc(block_size);
+    wrap_pread(fs_image, i_bitmap, block_size, bd_to_offest(bitmap_id));
 }
 
 void parse_inodes(struct ext2_super_block *super, struct ext2_group_desc *group) {
